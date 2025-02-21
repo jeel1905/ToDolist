67baa396e22303d25b9bb29dcbdf711e3989dc93
jeel1905 <jeelgabani19@gmail.com>
2025-02-21 11:30:07 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
-// import Student from './Student.jsx'
-// import React2 from "./React2.jsx";
-// import List from './List.jsx';
-// import State from './State.jsx';
-// import Form from './Form.jsx'
+
 import Todolist from './Todolist.jsx'
 function App() {
 
     return( <>     
-    {/* <h1>it's still working</h1>  */}
+    
     <Todolist/>
     </>
              
